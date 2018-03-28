9016bbdcbd91e3ee5574225b08a85d57ac515588
VToropov1337 <vladimirtoropov87@gmail.com>
2018-03-28 11:21:02 +0300

    save

---
+++ filereader.py
@@ -0,0 +1,24 @@
+import sys
+
+class FileReader:
+
+    def __init__(self, arg):
+        self.arg = sys.argv[1]
+
+    def read(self):
+        try:
+            with open(self.arg, "r") as f:
+                return f.read().strip()
+        except FileNotFoundError:
+            return "Отсутствует файл для чтения по адресу : {}".format(self.arg)
+        except IsADirectoryError:
+            return "Указана только директория. Укажите полный путь до файла."
+
+
+
+
+try:
+    reader = FileReader(sys.argv[1])
+    print(reader.read())
+except IndexError:
+    print("Отсутствует путь до файла для чтения")
