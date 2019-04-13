74110a9e8750c2c67966ce33aad77b1a94b7a955
VToropov1337 <vladimirtoropov87@gmail.com>
2019-04-13 21:51:55 +0300

    save

--- oop/MFTI-tasks/cars.py
+++ oop/MFTI-tasks/cars.py
@@ -7,7 +7,7 @@ class BaseCar(object):
         self.car_type = car_type
         self.brand = brand
         self.photo_file_name = photo_file_name
-        self.carrying = carrying
+        self.carrying = float(carrying)
         
     def __str__(self):
         return self.car_type
@@ -18,22 +18,27 @@ class BaseCar(object):
     
     def get_photo_file_ext(self):
         return os.path.splitext(self.photo_file_name)[1]
-        
-    
+            
     
 class Car(BaseCar):
-    def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
-        super().__init__(brand,photo_file_name,carrying)
+    def __init__(self, car_type,brand, photo_file_name, carrying, passenger_seats_count):
+        super().__init__(car_type,brand,photo_file_name,carrying)
         self.passenger_seats_count = passenger_seats_count
     
     
 class Truck(BaseCar):
     def __init__(self,car_type, brand, photo_file_name, carrying, body_whl=0):
         super().__init__(car_type,brand,photo_file_name,carrying)
-        self.body_whl = body_whl
-        self.body_width = int(self.body_whl.split('x')[0])
-        self.body_height = int(self.body_whl.split('x')[1])
-        self.body_lenght = int(self.body_whl.split('x')[2])
+        self.body_whl = 0 or body_whl
+        if body_whl != 0:
+            self.body_width = int(self.body_whl.split('x')[0])
+            self.body_height = int(self.body_whl.split('x')[1])
+            self.body_lenght = int(self.body_whl.split('x')[2])
+        else:
+            self.body_width = 0
+            self.body_height = 0
+            self.body_lenght = 0
+
                 
     def get_body_volume(self):
         sum = 1
@@ -46,31 +51,36 @@ class Truck(BaseCar):
         return sum
         
             
-class SpecMachone(BaseCar):
-    def __init__(self, brand, photo_file_name, carrying, extra):
-        super().__init__(brand,photo_file_name,carrying)
+class SpecMachine(BaseCar):
+    def __init__(self, car_type,brand, photo_file_name, carrying, extra):
+        super().__init__(car_type,brand,photo_file_name,carrying)
         self.extra = extra
         
-        
-        
-#подается файл-надо прочитать построчно и создать список нужных объектов. Функция должна возвращать список объектов        
+
 def get_car_list(csv_filename):
     car_list = []
+    with open(csv_filename,'r',newline="") as f:
+        reader = csv.reader(f)
+        next(reader)
+        # a = a[0].split(';')
+        for row in reader:
+            if len(row) == 0:
+                continue
+            else:
+                row = row[0].split(';')
+                if row[0] == 'car':
+                    car_list.append(Car(row[0],row[1],row[2],row[5],row[3]))
+                if row[0] == 'truck':
+                    car_list.append(Truck(row[0],row[1],row[3],row[5]))
+                if row[0] == 'spec_machine':
+                    car_list.append(SpecMachine(row[0],row[1],row[3],row[5],row[6]))
+
     return car_list
-    
-    
-    
-    
-    
-    
-r = Truck('truck','toyota','wer.jpg',20,'8x4x2')
 
-print(r.car_type,r.brand,r.carrying)
 
-print(r.get_photo_file_ext())
 
-print(r.get_body_volume())
+if __name__ == '__main__':
 
-print(r.body_lenght)
-
-print(r)
+    car_list = get_car_list('cars.csv')
+    for i in car_list:
+        print(i.__dict__)
